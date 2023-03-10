23b1edc6893b1c91d3a41e1da60455e4b590cf13
GabrielHRosa <gabriel.rosa17@fatec.sp.gov.br>
2023-03-10 19:48:42 -0300

    Criação da classe produto, alteração da classe program

--- ConsoleApp_noite/Program.cs
+++ ConsoleApp_noite/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_noite;
+
 Console.WriteLine("Hello, World!");
+
+
+Produto p1 = new Produto(1, 10.90M, "Cervejinha");
+
+Console.WriteLine("");
